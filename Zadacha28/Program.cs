ec045d83d70f21646c8a7c0279b033e984b8a7cf
Olga Varfolomeeva <romanova_85@mail.ru>
2023-01-16 21:49:13 +0300

    домашнее задание, задачи 25, 28, 29

---
+++ Zadacha28/Program.cs
@@ -0,0 +1,17 @@
+﻿int Sum(int number)
+{
+int sum = 0;
+int place1 = 0;
+
+for(int i = 0; i < number; i++)
+{
+    place1 = number - number % 10;
+    sum = sum + (number - place1);
+    number = number / 10;
+}
+return sum;
+}
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Sum(number));
